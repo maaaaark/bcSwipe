29178b87da6b8b9bbc0446feb5439d16aec4967e
mark <mshiraldi@gmail.com>
2015-05-28 10:50:24 -0700

    Initial code commit.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# bcSwipe
-A lightweight (less than 1kb) jQuery plugin to enable swipe gestures for Bootstrap 3 carousels, on iOS and Android.
+# bcSwipe (Bootstrap Carousel Swipe)
+Lightweight (> 1kb) jQuery plugin to enable swipe gestures for Bootstrap 3 carousels on iOS and Android.
+
+##Usage
+````javascript
+$('.carousel').bcSwipe({
+  threshold: 50,
+  preventDefaultEvents: true
+});
+````
+
+Adjusting threshold will determine how long a swipe must be to move to the next carousel slide.
---
+++ jquery.bcSwipe.js
@@ -0,0 +1,65 @@
+/**
+ * Bootstrap Carousel Swipe v1
+ *
+ * jQuery plugin to enable swipe gestures on Bootstrap 3 carousels.
+ * Examples and documentation: https://github.com/maaaaark/bcSwipe
+ *
+ * Licensed under the MIT license
+ * http://opensource.org/licenses/mit-license
+ */
+(function($) {
+  $.fn.bcSwipe = function(settings) {
+    var config = {
+      threshold: 50,
+      preventDefaultEvents: true
+    };
+
+    if (settings) {
+      $.extend(config, settings);
+    }
+
+    this.each(function() {
+      var stillMoving = false;
+      var start;
+
+      if ('ontouchstart' in document.documentElement) {
+        this.addEventListener('touchstart', onTouchStart, false);
+      }
+
+      function onTouchStart(e) {
+        if (e.touches.length == 1) {
+          start = e.touches[0].pageX;
+          stillMoving = true;
+          this.addEventListener('touchmove', onTouchMove, false);
+        }
+      }
+
+      function onTouchMove(e) {
+        if (config.preventDefaultEvents) {
+          e.preventDefault();
+        }
+        if (stillMoving) {
+          var x = e.touches[0].pageX;
+          var difference = start - x;
+          if (Math.abs(difference) >= config.threshold) {
+            cancelTouch();
+            if (difference > 0) {
+              $(this).carousel('next');
+            }
+            else {
+              $(this).carousel('prev');
+            }
+          }
+        }
+      }
+
+      function cancelTouch() {
+        this.removeEventListener('touchmove', onTouchMove);
+        start = null;
+        stillMoving = false;
+      }
+    });
+
+    return this;
+  };
+})(jQuery);
